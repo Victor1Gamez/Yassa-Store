55cf633e5067d0943858814ecf52fb01c8708c9c
Victor1Gamez <146119148+Victor1Gamez@users.noreply.github.com>
2024-12-01 12:19:43 -0500

    agregue 1

---
+++ script.js
@@ -0,0 +1,38 @@
+var swiper = new Swiper(".mySwiper-1",{
+    slidesPerView:1,
+    soaceBetween: 30,
+    loop: true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+    },
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+});
+
+
+var swiper = new Swiper(".mySwiper-2",{
+    slidesPerView:3,
+    soaceBetween: 20,
+    loop: true,
+    loopFillGroupWithBlank: true,
+
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    },
+    breakpoints : {
+        0: {
+            slidesPerView:1,
+        },
+        520: {
+            slidesPerView:2,
+        },
+        850: {
+            slidesPerView:3,
+        }
+    }
+});
+
